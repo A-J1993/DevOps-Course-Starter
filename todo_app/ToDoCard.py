948893828d4145a7d8a49e2bdb23d0e65519fdf6
Ali-John Mirsepassi <alijohnpassi@gmail.com>
2020-12-17 12:26:35 +0000

    added in status filter for ViewModel

--- todo_app/ToDoCard.py
+++ todo_app/ToDoCard.py
@@ -1,7 +1,26 @@
 "establishes the ToDoCard class"
 
+import os
+import requests
+
+from todo_app.trello_config import TODOID
+
 class ToDoCard():
 
-    def __init__(self, id, name):
+    def __init__(self, id, name, status = "To Do"):
         self.id = id
         self.name = name
+        self.status = status
+
+    @classmethod
+    def from_trello_card(cls, card):
+        status = ""
+        if card["listId"] ==  TODOID:
+            status = "To Do"
+        else:
+            status = "Done"
+        return cls(
+            card['id'],
+            card['name'],
+            status
+    )
--- todo_app/ViewModel.py
+++ todo_app/ViewModel.py
@@ -5,3 +5,11 @@ class ViewModel:
     @property
     def items(self):
         return self._items
+
+    def to_do_items(self):
+        filtered_items = [x for x in self._items if x.status == "To Do"]
+        return filtered_items
+
+    def done_items(self):
+        filtered_items = [x for x in self._items if x.status == "Done"]
+        return filtered_items

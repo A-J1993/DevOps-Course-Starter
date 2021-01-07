0cbd0ef52c14d1fbff9d8c541609d8717998190b
Ali-John Mirsepassi <alijohnpassi@gmail.com>
2021-01-07 13:26:09 +0000

    Added in Assertation Tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,6 +8,72 @@ from dotenv import find_dotenv, load_dotenv
 
 from unittest.mock import patch 
 
+#Allows one to use json values of null and false in a Python framework
+null = None
+false = False
+
+sample_trello_lists_response = [
+    {
+        "id": "5fdcda2428d71e74db64410e",
+        "checkItemStates": null,
+        "closed": false,
+        "dateLastActivity": "2020-12-18T16:34:44.809Z",
+        "desc": "",
+        "descData": null,
+        "dueReminder": null,
+        "idBoard": "555555555555555555555555",
+        "idList": "333333333333333333333333",
+        "idMembersVoted": [],
+        "idShort": 12,
+        "idAttachmentCover": null,
+        "idLabels": [],
+        "manualCoverAttachment": false,
+        "name": "Why does this not work",
+        "pos": 147456,
+        "shortLink": "pLobkTfQ",
+        "isTemplate": false,
+        "cardRole": null,
+        "badges": {
+            "attachmentsByType": {
+                "trello": {
+                    "board": 0,
+                    "card": 0
+                }
+            },
+            "location": false,
+            "votes": 0,
+            "viewingMemberVoted": false,
+            "subscribed": false,
+            "fogbugz": "",
+            "checkItems": 0,
+            "checkItemsChecked": 0,
+            "checkItemsEarliestDue": null,
+            "comments": 0,
+            "attachments": 0,
+            "description": false,
+            "due": null,
+            "dueComplete": false,
+            "start": null
+        },
+        "dueComplete": false,
+        "due": null,
+        "idChecklists": [],
+        "idMembers": [],
+        "labels": [],
+        "shortUrl": "https://trello.com/c/pLobkTfQ",
+        "start": null,
+        "subscribed": false,
+        "url": "https://trello.com/c/pLobkTfQ/12-why-does-this-not-work",
+        "cover": {
+            "idAttachment": null,
+            "color": null,
+            "idUploadedBackground": null,
+            "size": "normal",
+            "brightness": "light"
+        }
+    }
+]
+
 @pytest.fixture
 def client():
     # Use out latest integration config instead of the 'real' version
@@ -24,14 +90,14 @@ def client():
 @patch('requests.get')
 def test_index_page(mock_get_requests, client):
     #Replace call to requests.get(url) with our own function
-    mock_get_requests.side_effects = mock_get_lists
+    mock_get_requests.side_effects = mock_get_cards
     response = client.get('/trello')
+    assert response.status_code == 200
 
-def mock_get_lists(url, params):
+def mock_get_cards(url, params):
     if url == f'https://api.trello.com/1/boards/{TEST_BOARD_ID}/lists':
         response = Mock()
         # sample_trello_lists_response should point to some test reponse data
-        response.json.return_value = sample_trello_lists_response
+        response.json.return_value = sample_trello_cards_response
         return response
     return None
-    #assert response.status_code == 200

c7df384c7c5d5fd503a487f7970ea451275d94b9
Ali-John Mirsepassi <alijohnpassi@gmail.com>
2021-01-07 16:17:49 +0000

    Added in test_app function code as well as installed selenium

--- tests/test_app.py
+++ tests/test_app.py
@@ -116,3 +116,24 @@ def create_trello_board():
 def delete_trello_board(trello_board_id):
     params = {"key": os.getenv("TRELLO_KEY"), "token": os.getenv("TRELLO_TOKEN")}
     requests.delete("https://api.trello.com/1/boards/" + trello_board_id, data = params)
+
+@pytest.fixture(scope = 'module')
+def test_app():
+    #Create the new board and update the board id enviroment variable 
+    board_id = create_trello_board()
+    os.environ['TRELLO BOARD ID'] = board_id
+
+    #construct the new application
+    application = app.create_app
+
+    #start the app in its own thread.
+    thread = Thread(target =lambda: application.run(use_reloader = False))
+    thread.daemon = True
+    thread.start()
+    yield app
+
+    #Tear Down
+    thread.join(1)
+    delete_trello_board(board_id)
+
+    
